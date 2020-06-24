2829833d6d00f803aced2dedf22029d39efe383f
Wasim <wasimahmad.qc@gmail.com>
2020-06-24 18:26:23 +0200

    DeepAR Implementation for time series forecasting

---
+++ deepAR.py
@@ -0,0 +1,49 @@
+from gluonts.dataset import common
+from gluonts.model import deepar
+from gluonts.trainer import Trainer
+from gluonts.evaluation import Evaluator
+import crps
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+"Load electricity dataset"
+
+Edf = pd.read_csv('datasets/electricity/electricity.csv', header=0, index_col=0)
+# url = "https://raw.githubusercontent.com/numenta/NAB/master/data/realTweets/Twitter_volume_AMZN.csv"
+# df = pd.read_csv(url, header=0, index_col=0)
+data = common.ListDataset([{
+    "start": Edf.index[0],
+    "target": Edf.MT_003[:44100]
+}],
+                          freq="5min")
+
+trainer = Trainer(epochs=50)
+estimator = deepar.DeepAREstimator(
+    freq="5min", prediction_length=24, trainer=trainer)
+predictor = estimator.train(training_data=data)
+
+
+actual = Edf.MT_003[44100:44124].values.tolist()
+
+prediction = next(predictor.predict(data))
+eval = Evaluator()
+forecast = prediction.mean
+
+print("Forecast shape: ", forecast.shape)
+print("Actual shape: ", len(actual))
+print(prediction.mean)
+print("MAPE: ", eval.mape(actual, forecast))
+print("MSE: ", eval.mse(actual, forecast))
+print("CRPS: ", crps.calc_crps(forecast, actual))
+
+prediction.plot(output_file='plots/graph.png')
+
+compare_df = pd.DataFrame({'Actual': actual, 'Forecast': forecast})
+
+# plot the two vectors
+ax = compare_df.plot(colormap='jet', marker='.', markersize=10, title='Forecasting Electricity Consumption')
+ax.set_xlabel("Time frequency")
+ax.set_ylabel("Electricity consumption")
+fig = ax.get_figure()
+fig.savefig("plots/compare.png")
