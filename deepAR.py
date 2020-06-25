b05affd227f44e01625ba904778e88e22b44f25f
Wasim <wasimahmad.qc@gmail.com>
2020-06-25 18:32:38 +0200

    extracted NC variables from dataset

--- deepAR.py
+++ deepAR.py
@@ -2,18 +2,34 @@ from gluonts.dataset import common
 from gluonts.model import deepar
 from gluonts.trainer import Trainer
 from gluonts.evaluation import Evaluator
+from netCDF4 import Dataset
 import confidence
 import numpy as np
 import crps
-
 import pandas as pd
 import matplotlib.pyplot as plt
+import netCDF
+
+
+"Load NC data"
+nc_f = 'datasets/ncdata/DE_Hai2000_2012.nc'  # Your filename
+nc_fid = Dataset(nc_f, 'r')   # Dataset is the class behavior to open the file                         # and create an instance of the ncCDF4 class
+nc_attrs, nc_dims, nc_vars = netCDF.ncdump(nc_fid);
+
+# Extract data from NetCDF file
+vpd_f = nc_fid.variables['VPD_F'][:]  # extract/copy the data
+tair_f = nc_fid.variables['TA_F'][:]
+# rg_f = nc_fid.variables['Rg_f'][:]
+swc1_f = nc_fid.variables['SWC_F_MDS_1'][:]
+nee_f = nc_fid.variables['NEE_CUT_REF'][:]
+gpp_f = nc_fid.variables['GPP_NT_VUT_REF'][:]
+reco = nc_fid.variables['RECO_DT_VUT_REF'][:]
+le_f = nc_fid.variables['H_F_MDS'][:]
+h_f = nc_fid.variables['H_F_MDS'][:]
+time = nc_fid.variables['time'][:]
 
 "Load electricity dataset"
-
 Edf = pd.read_csv('datasets/electricity/electricity.csv', header=0, index_col=0)
-# url = "https://raw.githubusercontent.com/numenta/NAB/master/data/realTweets/Twitter_volume_AMZN.csv"
-# df = pd.read_csv(url, header=0, index_col=0)
 data = common.ListDataset([{
     "start": Edf.index[0],
     "target": Edf.MT_003[:44100]
@@ -33,7 +49,6 @@ forecast = prediction.mean
 print(prediction.mean)
 print("MAPE: ", eval.mape(actual, forecast))
 print("MSE: ", eval.mse(actual, forecast))
-print("RMSE: ", np.roots(eval.mse(actual, forecast)))
 print("CRPS: ", crps.calc_crps(forecast, actual))
 
 prediction.plot(output_file='plots/graph.png')
