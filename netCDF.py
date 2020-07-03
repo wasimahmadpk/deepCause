21efec4091f0c9894082db349431595f6ae7426a
Wasim <wasimahmad.qc@gmail.com>
2020-07-03 16:59:09 +0200

    Restricted printing variables

--- netCDF.py
+++ netCDF.py
@@ -63,28 +63,28 @@ def ncdump(nc_fid, verb=True):
 
     # NetCDF global attributes
     nc_attrs = nc_fid.ncattrs()
-    if verb:
-        print("NetCDF Global Attributes:")
-        for nc_attr in nc_attrs:
-            print('\t%s:' % nc_attr, repr(nc_fid.getncattr(nc_attr)))
+    # if verb:
+    #     print("NetCDF Global Attributes:")
+    #     for nc_attr in nc_attrs:
+    #         print('\t%s:' % nc_attr, repr(nc_fid.getncattr(nc_attr)))
     nc_dims = [dim for dim in nc_fid.dimensions]  # list of nc dimensions
-    # Dimension shape information.
-    if verb:
-        print("NetCDF dimension information:")
-        for dim in nc_dims:
-            print("\tName:", dim)
-            print("\t\tsize:", len(nc_fid.dimensions[dim]))
-            print_ncattr(dim)
+    # # Dimension shape information.
+    # if verb:
+    #     print("NetCDF dimension information:")
+    #     for dim in nc_dims:
+    #         print("\tName:", dim)
+    #         print("\t\tsize:", len(nc_fid.dimensions[dim]))
+    #         print_ncattr(dim)
     # Variable information.
     nc_vars = [var for var in nc_fid.variables]  # list of nc variables
-    if verb:
-        print("NetCDF variable information:")
-        for var in nc_vars:
-            if var not in nc_dims:
-                print('\tName:', var)
-                print("\t\tdimensions:", nc_fid.variables[var].dimensions)
-                print("\t\tsize:", nc_fid.variables[var].size)
-                print_ncattr(var)
+    # if verb:
+    #     print("NetCDF variable information:")
+    #     for var in nc_vars:
+    #         if var not in nc_dims:
+    #             print('\tName:', var)
+    #             print("\t\tdimensions:", nc_fid.variables[var].dimensions)
+    #             print("\t\tsize:", nc_fid.variables[var].size)
+    #             print_ncattr(var)
     return nc_attrs, nc_dims, nc_vars
 
 nc_f = 'datasets/ncdata/DE_Hai2000_2012.nc'  # Your filename
