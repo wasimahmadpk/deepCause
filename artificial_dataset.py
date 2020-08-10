0d8ac1fc02fa60827ab8ec99dc1483aa1072f9f0
Wasim <wasimahmad.qc@gmail.com>
2020-08-10 15:52:34 +0200

    Generate synthetic data from global radiation

--- artificial_dataset.py
+++ artificial_dataset.py
@@ -1,8 +1,10 @@
-def artificial_dataset(Rg):
+def artificial_dataset(Rg, time_steps, Tref, C, Tao, et, egpp, ereco):
     Rg = Rg
-
+    # c1, c2, c3, c4, c5 = 0.3, 0.4, 0.5, 0.6, 1.5
+    T, Gpp, Reco = [], [], []
     def generate_data(Rg):
-        T = Rg * Rg
-        Gpp = Rg.T
-        Reco = Gpp * Rg * T
-        return Rg, T, Gpp, Reco
+        for t in time_steps:
+            T.append(C.c1*T[t-Tao.t1] + C.c2*Rg(t-Tao.t2) + et)
+            Gpp.append(C.c3*Rg(t-Tao.t3)*T(t-Tao.t4) + egpp)
+            Reco.append(C.c4*Gpp(t-Tao.t5)*C.c5^(T(t-Tao.t6)-Tref) + ereco)
+            return Rg, T, Gpp, Reco
