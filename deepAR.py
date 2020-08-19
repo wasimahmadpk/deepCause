b84591bcd3379cfc7b43fb99e6bc55884b8c8efb
Wasim <wasimahmad.qc@gmail.com>
2020-08-19 17:09:27 +0200

    Modelling reco from VPD

--- deepAR.py
+++ deepAR.py
@@ -50,7 +50,7 @@ year = nc_fid.variables['year'][:].ravel().data
 train_ds = ListDataset(
     [
          {'start': "07/01/2006 00:00:00", 'target': reco[start:train_stop],
-          'dynamic_feat':[gpp[start:train_stop]]}
+          'dynamic_feat':[vpd[start:train_stop]]}
         # {'start': "01/01/2006 00:00:00", 'target': temp[start:train_stop], 'cat': [1],
         #  'dynamic_feat':[reco[start:train_stop], rg[start:train_stop], gpp[start:train_stop]]},
         # {'start': "01/01/2006 00:00:00", 'target': rg[start:train_stop], 'cat': [2],
@@ -64,7 +64,7 @@ train_ds = ListDataset(
 test_ds = ListDataset(
     [
         {'start': "07/01/2006 00:00:00", 'target': reco[start:test_stop],
-         'dynamic_feat':[gpp[start:train_stop]]}
+         'dynamic_feat':[vpd[start:train_stop]]}
         # {'start': "01/01/2006 00:00:00", 'target': temp[start:test_stop], 'cat': [1],
         #  'dynamic_feat': [reco[start:test_stop], rg[start:test_stop], gpp[start:train_stop]]},
         # {'start': "01/01/2006 00:00:00", 'target': rg[start:test_stop], 'cat': [2],
@@ -107,7 +107,7 @@ def plot_forecasts(tss, forecasts, past_length, num_plots):
         forecast.plot(color='g')
         plt.grid(which='both')
         plt.legend(["observations", "median prediction", "90% confidence interval", "50% confidence interval"])
-        plt.title("Forecasting " + titles[counter] + " time series")
+        plt.title("Forecasting " + titles[counter] + " time series from VPD")
         plt.xlabel("Timestamp")
         plt.ylabel(titles[counter])
         plt.show()
