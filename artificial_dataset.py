7f0ebcf8506176c81782be83bf15b68d009b79b4
Wasim <wasimahmad.qc@gmail.com>
2020-08-21 18:50:50 +0200

    Generate artificial data

--- artificial_dataset.py
+++ artificial_dataset.py
@@ -1,4 +1,8 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from netCDF4 import Dataset
+import netCDF
 
 class artificial_dataset():
 
@@ -13,7 +17,7 @@ class artificial_dataset():
         self.egpp = egpp
         self.ereco = ereco
         # c1, c2, c3, c4, c5 = 0.3, 0.4, 0.5, 0.6, 1.5
-        self.T, self.Gpp, self.Reco = [1, 2, 4, 3, 3, 4, 5, 6], [1, 2, 3, 3,9, 4, 5, 6], [1, 2, 3, 3, 3, 4, 5, 6]
+        self.T, self.Gpp, self.Reco = rg[0:10], rg[0:10], rg[0:10]
 
     def generate_data(self):
 
@@ -24,12 +28,21 @@ class artificial_dataset():
             self.Reco.append(C.get('c4')*self.Gpp[(t+6)-Tao.get('t5')]*C.get('c5')**(self.T[(t+6)-Tao.get('t6')]-Tref) + ereco)
         return Rg, self.T, self.Gpp, self.Reco
 
+
 if __name__ == '__main__':
-    # c1, c2, c3, c4, c5 = 0.3, 0.4, 0.5, 0.6, 1.5
-    time_steps, Tref = 50, 29
-    Rg = np.ones(time_steps+2).tolist()
-    et, egpp, ereco = 1, 2, 3
-    C = {'c1': 0.1, 'c2': 0.2, 'c3': 0.3, 'c4': 0.4, 'c5': 1}
-    Tao = {'t1': 1, 't2': 2, 't3': 3, 't4': 4, 't5': 5, 't6': 6}
+
+    "Load fluxnet data"
+    nc_f = '/home/ahmad/PycharmProjects/deepCause/datasets/ncdata/DE-Hai.2000.2006.hourly.nc'  # Your filename
+    nc_fid = Dataset(nc_f, 'r')
+    nc_attrs, nc_dims, nc_vars = netCDF.ncdump(nc_fid);
+
+    # Extract data from NetCDF file
+    rg = list(nc_fid.variables['Rg_f'][:].ravel().data)
+
+    time_steps, Tref = len(rg), 15
+    Rg = rg
+    et, egpp, ereco = 1, 2, 5
+    C = {'c1': 0.2, 'c2': 0.4, 'c3': 0.6, 'c4': 0.4, 'c5': 1.5}
+    Tao = {'t1': 1, 't2': 3, 't3': 5, 't4': 7, 't5': 9, 't6': 11}
     data_obj = artificial_dataset(Rg, time_steps, Tref, C, Tao, et, egpp, ereco)
     rg, tair, gpp, reco = data_obj.generate_data()
