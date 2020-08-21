d84cf44c36b91c4aadfe996fb7311483c21aca20
Wasim <wasimahmad.qc@gmail.com>
2020-08-21 17:32:57 +0200

    Generate artificial data

--- artificial_dataset.py
+++ artificial_dataset.py
@@ -1,10 +1,35 @@
-def artificial_dataset(Rg, time_steps, Tref, C, Tao, et, egpp, ereco):
-    Rg = Rg
+import numpy as np
+
+class artificial_dataset():
+
+    def __init__(self, Rg, time_steps, Tref, C, Tao, et, egpp, ereco):
+
+        self.time_steps = time_steps
+        self.Rg = Rg
+        self.Tref = Tref
+        self.C = C
+        self.Tao = Tao
+        self.et = et
+        self.egpp = egpp
+        self.ereco = ereco
+        # c1, c2, c3, c4, c5 = 0.3, 0.4, 0.5, 0.6, 1.5
+        self.T, self.Gpp, self.Reco = [1, 2, 4, 3, 3, 4, 5, 6], [1, 2, 3, 3,9, 4, 5, 6], [1, 2, 3, 3, 3, 4, 5, 6]
+
+    def generate_data(self):
+
+        for t in range(self.time_steps-6):
+            print(t)
+            self.T.append(C.get('c1')*self.T[(t+6)-Tao.get('t1')] + C.get('c2')*Rg[(t+6)-Tao.get('t2')] + et)
+            self.Gpp.append(C.get('c3')*Rg[(t+6)-Tao.get('t3')]*self.T[(t+6)-Tao.get('t4')] + egpp)
+            self.Reco.append(C.get('c4')*self.Gpp[(t+6)-Tao.get('t5')]*C.get('c5')**(self.T[(t+6)-Tao.get('t6')]-Tref) + ereco)
+        return Rg, self.T, self.Gpp, self.Reco
+
+if __name__ == '__main__':
     # c1, c2, c3, c4, c5 = 0.3, 0.4, 0.5, 0.6, 1.5
-    T, Gpp, Reco = [], [], []
-    def generate_data(Rg):
-        for t in time_steps:
-            T.append(C.c1*T[t-Tao.t1] + C.c2*Rg(t-Tao.t2) + et)
-            Gpp.append(C.c3*Rg(t-Tao.t3)*T(t-Tao.t4) + egpp)
-            Reco.append(C.c4*Gpp(t-Tao.t5)*C.c5^(T(t-Tao.t6)-Tref) + ereco)
-            return Rg, T, Gpp, Reco
+    time_steps, Tref = 50, 29
+    Rg = np.ones(time_steps+2).tolist()
+    et, egpp, ereco = 1, 2, 3
+    C = {'c1': 0.1, 'c2': 0.2, 'c3': 0.3, 'c4': 0.4, 'c5': 1}
+    Tao = {'t1': 1, 't2': 2, 't3': 3, 't4': 4, 't5': 5, 't6': 6}
+    data_obj = artificial_dataset(Rg, time_steps, Tref, C, Tao, et, egpp, ereco)
+    rg, tair, gpp, reco = data_obj.generate_data()
