f865387ab6bd8165ee876ac8c86ec7ba4c5c5aa3
Wasim <wasimahmad.qc@gmail.com>
2020-08-27 18:18:15 +0200

    Modelling Reco using univariate deepAR model

--- deepAR.py
+++ deepAR.py
@@ -18,7 +18,7 @@ epochs = 100
 training_length = 3000  # data for 2 month (July)
 prediction_length = 144  # data for 3 days
 
-start = 86000
+start = 8640
 train_stop = start + training_length
 test_stop = train_stop + prediction_length
 # ******************************************************************
@@ -59,8 +59,8 @@ reco = fluxnet['RECO_NT_VUT_50']
 
 train_ds = ListDataset(
     [
-         {'start': "07/01/2003 00:00:00", 'target': reco[start:train_stop],
-          'dynamic_feat':[temp[start:train_stop]]}
+         {'start': "07/01/2003 00:00:00", 'target': reco[start:train_stop]}
+          # 'dynamic_feat':[temp[start:train_stop]]}
         # {'start': "01/01/2006 00:00:00", 'target': temp[start:train_stop], 'cat': [1],
         #  'dynamic_feat':[reco[start:train_stop], rg[start:train_stop], gpp[start:train_stop]]},
         # {'start': "01/01/2006 00:00:00", 'target': rg[start:train_stop], 'cat': [2],
@@ -73,8 +73,8 @@ train_ds = ListDataset(
 
 test_ds = ListDataset(
     [
-        {'start': "07/01/2003 00:00:00", 'target': reco[start:test_stop],
-         'dynamic_feat':[temp[start:test_stop]]}
+        {'start': "07/01/2003 00:00:00", 'target': reco[start:test_stop]}
+         # 'dynamic_feat':[temp[start:test_stop]]}
         # {'start': "01/01/2006 00:00:00", 'target': temp[start:test_stop], 'cat': [1],
         #  'dynamic_feat': [reco[start:test_stop], rg[start:test_stop], gpp[start:train_stop]]},
         # {'start': "01/01/2006 00:00:00", 'target': rg[start:test_stop], 'cat': [2],
