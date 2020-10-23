5c9a5515d747dc9e01ddb1625804f07f19cf701a
Wasim <wasimahmad.qc@gmail.com>
2020-10-23 19:03:20 +0200

    FInding causal effect using deepAR

--- deepAR.py
+++ deepAR.py
@@ -8,6 +8,7 @@ from os import path
 from math import sqrt
 from netCDF4 import Dataset
 from datetime import datetime
+from fit_dist import fitDist
 import matplotlib.pyplot as plt
 from itertools import islice
 from gluonts.evaluation import Evaluator
@@ -15,6 +16,7 @@ from gluonts.dataset.common import ListDataset
 from gluonts.model.deepar import DeepAREstimator
 from gluonts.model.deepar._network import DeepARTrainingNetwork
 from gluonts.trainer import Trainer
+from model_test import modelTest
 from sklearn.metrics import mean_squared_error
 from gluonts.distribution.multivariate_gaussian import MultivariateGaussianOutput
 from gluonts.evaluation.backtest import make_evaluation_predictions
@@ -96,7 +98,6 @@ otemp = syndata['T']
 ogpp = syndata['GPP']
 oreco = syndata['Reco']
 
-
 # ************ Normalize the features *************
 rg = normalize(down_sample(org, win_size))
 temp = normalize(down_sample(otemp, win_size))
@@ -104,6 +105,16 @@ gpp = normalize(down_sample(ogpp, win_size))
 reco = normalize(down_sample(oreco, win_size))
 # vpd = normalize(down_sample(ovpd, win_size))
 # ppt = normalize(down_sample(oppt, win_size))
+
+# **********Fit distribution **********************
+dfname = pathlib.Path("tseries.dist")
+# fdist = fitDist(temp)
+# with open(dfname, 'wb') as f:
+#     pickle.dump(fdist, f)
+
+with open(dfname, 'rb') as f:
+    fdist = pickle.load(f)
+
 intervene = np.random.choice(temp, len(reco))
 
 
@@ -175,49 +186,60 @@ if not filename.exists():
     pickle.dump(predictor, open(filename, 'wb'))
 
 
-# load the model from disk
-predictor = pickle.load(open(filename, 'rb'))
+# test model
+rmselist = []
+mapelist = []
 
-forecast_it, ts_it = make_evaluation_predictions(
-    dataset=test_ds,  # test dataset
-    predictor=predictor,  # predictor
-    num_samples=num_samples,  # number of sample paths we want for evaluation
-)
+for i in range(10):
+    rmse, mape = modelTest(test_ds, num_samples, reco, train_stop, test_stop)
+    rmselist.append(rmse)
+    mapelist.append(mape)
 
+rmse = np.mean(rmselist)
+mape = np.mean(mapelist)
+# # load the model from disk
+# predictor = pickle.load(open(filename, 'rb'))
+#
+# forecast_it, ts_it = make_evaluation_predictions(
+#     dataset=test_ds,  # test dataset
+#     predictor=predictor,  # predictor
+#     num_samples=num_samples,  # number of sample paths we want for evaluation
+# )
+#
+#
+# def plot_forecasts(tss, forecasts, past_length, num_plots):
+#
+#     for target, forecast in islice(zip(tss, forecasts), num_plots):
+#
+#         ax = target[-past_length:][0].plot(figsize=(14, 10), linewidth=2)
+#         forecast.copy_dim(0).plot(color='g')
+#         plt.grid(which='both')
+#         plt.legend(["observations", "median prediction", "90% confidence interval", "50% confidence interval"])
+#         plt.title("Forecasting Reco time series")
+#         plt.xlabel("Timestamp")
+#         plt.ylabel('Reco')
+#         plt.show()
+#
+#
+# forecasts = list(forecast_it)
+# tss = list(ts_it)
+#
+# y_pred = []
+#
+# for i in range(num_samples):
+#     y_pred.append(forecasts[0].samples[i].transpose()[0].tolist())
+#
+# y_pred = np.array(y_pred)
+# y_true = reco[train_stop: test_stop]
+# mape = mean_absolute_percentage_error(y_true, np.mean(y_pred, axis=0))
+#
+# rmse = sqrt(mean_squared_error(y_true, np.mean(y_pred, axis=0)))
 
-def plot_forecasts(tss, forecasts, past_length, num_plots):
-
-    for target, forecast in islice(zip(tss, forecasts), num_plots):
-
-        ax = target[-past_length:][0].plot(figsize=(14, 10), linewidth=2)
-        forecast.copy_dim(0).plot(color='g')
-        plt.grid(which='both')
-        plt.legend(["observations", "median prediction", "90% confidence interval", "50% confidence interval"])
-        plt.title("Forecasting Reco time series")
-        plt.xlabel("Timestamp")
-        plt.ylabel('Reco')
-        plt.show()
-
-
-forecasts = list(forecast_it)
-tss = list(ts_it)
-
-y_pred = []
-
-for i in range(num_samples):
-    y_pred.append(forecasts[0].samples[i].transpose()[0].tolist())
-
-y_pred = np.array(y_pred)
-y_true = reco[train_stop: test_stop]
-mape = mean_absolute_percentage_error(y_true, np.mean(y_pred, axis=0))
-
-rmse = sqrt(mean_squared_error(y_true, np.mean(y_pred, axis=0)))
 print(f"RMSE: {rmse}, MAPE:{mape}%")
 print("Causal strength: ", math.log(rmse/0.1690), 2)
 
-plot_forecasts(tss, forecasts, past_length=33, num_plots=4)
-
-evaluator = Evaluator(quantiles=[0.1, 0.5, 0.9])
-agg_metrics, item_metrics = evaluator(iter([pd.DataFrame((tss[0][:][0]))]), iter([forecasts[0].copy_dim(0)]), num_series=len(test_ds))
-print("Intervention on Temperature")
-print("Performance metrices", agg_metrics)
+# plot_forecasts(tss, forecasts, past_length=33, num_plots=4)
+#
+# evaluator = Evaluator(quantiles=[0.1, 0.5, 0.9])
+# agg_metrics, item_metrics = evaluator(iter([pd.DataFrame((tss[0][:][0]))]), iter([forecasts[0].copy_dim(0)]), num_series=len(test_ds))
+# print("Performance metrices", agg_metrics)
